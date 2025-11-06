c4113adb319b76757460be88337dd7a8ec353dfe
Manuel Zuñiga <manuelalexis68@hotmail.com>
2025-11-06 10:28:09 -0300

    first commit

--- assets/js/nav.js
+++
@@ -1,8 +0,0 @@
-(function(){
-const links = document.querySelectorAll('a[data-nav]');
-const here = location.pathname.split('/').pop() || 'index.html';
-links.forEach(a => {
-const file = a.getAttribute('href');
-if(file === here){ a.classList.add('active'); }
-});
-})();
